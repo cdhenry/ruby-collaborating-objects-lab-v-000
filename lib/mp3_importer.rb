7532afd15335fd216eaddaa8a4071cf1441ad4c8
Chris Henry <cdhenry@gmail.com>
2018-02-15 00:41:20 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,7 +6,7 @@ class MP3Importer
   end
 
   def files
-    Dir["#{path}/*.mp3"].collect{|file| file.gsub(path + "/" ,'')}
+    @files ||= Dir["#{path}/*.mp3"].collect{|file| file.gsub(path + "/" ,'')}
   end
 
   def import
